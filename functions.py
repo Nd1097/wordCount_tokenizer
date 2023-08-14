b6ad7a8d096a9cc153ffdb85530d92114b7b021a
Nayan Dixit <ndixit@edelson.com>
2023-08-14 11:15:05 -0500

    function added

---
+++ functions.py
@@ -0,0 +1,80 @@
+import os
+import json
+import tiktoken
+
+
+# Writing the encoding function, which calculates token length for the given string
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+
+# Counting tokens for chat completion API calls
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+# Calculate the price for the api call with openai for the given model Name:
+
+def get_price(model_name):
+    """Gets the price for a single call for a given model"""
+    price_file = os.path.join(os.path.dirname(__file__),"model_prices.json")
+    with open(price_file, "r") as f:
+        prices = json.load(f)
+
+    if model_name not in prices:
+        raise ValueError(f"Model {model_name} not in the price list")
+    
+    return prices[model_name]
+
+
+def calculate_cost(token_len:int, model_name:str):
+    """Calculate the cost by the number of tokens in a prompt"""
+    price = get_price(model_name)
+    total_cost = (token_len/1000)*price
+    return total_cost
+
+
+# print(calculate_cost(540,"gpt-4-32k"))
